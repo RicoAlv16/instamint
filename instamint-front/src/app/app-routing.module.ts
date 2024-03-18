5f4bbb4f0dee22395c0c66e134183f802cfd4e83
Ulysse VIGNOLET <vignolet.u@reside-etudes.fr>
2024-03-18 10:57:25 +0100

    Revert "I can log in to my account, when I enter my e-mail and password."
    This reverts commit a593cb0042c4978eae73bdd160a2c9a1dc080930.

--- instamint-front/src/app/app-routing.module.ts
+++ instamint-front/src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./public/public.module').then(m => m.PublicModule),
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
