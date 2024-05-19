dc6627b21b7835c24ea0a1bf1ac8e2486f363cb5
Yann Wulfman <yann.wulfman@gmail.com>
2024-05-19 18:30:53 +0200

    conflicts fixing

--- instamint-front/src/app/app-routing.module.ts
+++ instamint-front/src/app/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostFeedComponent } from './features/post-feed/post-feed.component';
 
-const routes: Routes = [
-  { path: 'postfeed', component: PostFeedComponent },
-  { path: '', redirectTo: '/postfeed', pathMatch: 'full' },
-  { path: '**', redirectTo: '/postfeed' }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
